99005cca14510e6a8cf42d743e2223d4a552fe57
Yan Gorski <gorski.yan.93@gmail.com>
2026-02-03 22:40:55 -0300

    Initial Commit - Project from Spring Initalizr

---
+++ src/main/java/com/gorski/weather_are_you/WeatherAreYouApplication.java
@@ -0,0 +1,13 @@
+package com.gorski.weather_are_you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAreYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAreYouApplication.class, args);
+	}
+
+}
